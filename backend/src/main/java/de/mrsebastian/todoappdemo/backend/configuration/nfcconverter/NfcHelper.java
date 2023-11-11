f6a731399a33fe81aaeb5f3bfd2117005d1bd292
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2023-11-11 23:21:15 +0100

    remove unused import

--- backend/src/main/java/de/mrsebastian/todoappdemo/backend/configuration/nfcconverter/NfcHelper.java
+++ backend/src/main/java/de/mrsebastian/todoappdemo/backend/configuration/nfcconverter/NfcHelper.java
@@ -12,7 +12,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
