61ac7ec1ec0082aea2f080297fe1390ed48f49df
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2024-02-05 21:23:51 +0100

    first draft of userinfo menu component

---
+++ frontend/frontend/src/features/user/components/UserInfoMenu.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-menu>
+        <template #activator="{ props }">
+            <v-btn
+                icon
+                v-bind="props"
+            >
+                <v-icon>mdi-account-circle</v-icon>
+            </v-btn>
+        </template>
+
+        <v-card>
+            <v-card-subtitle>{{
+                userStore.userState.username
+            }}</v-card-subtitle>
+            <v-card-text>
+                <div>Roles</div>
+                <v-list>
+                    <v-list-item
+                        v-for="role in userStore.userState.roles"
+                        :key="role"
+                    >
+                        {{ role }}
+                    </v-list-item>
+                </v-list>
+            </v-card-text></v-card
+        >
+    </v-menu>
+</template>
+
+<script setup lang="ts">
+import { useUserStore } from "@/stores/user";
+
+const userStore = useUserStore();
+</script>
