b4d4bea1a5627b62ff96fa61bfab8dd320ef25ca
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2024-02-06 20:43:39 +0100

    change design of roles in menu

--- frontend/frontend/src/features/user/components/UserInfoMenu.vue
+++ frontend/frontend/src/features/user/components/UserInfoMenu.vue
@@ -10,21 +10,18 @@
         </template>
 
         <v-card>
-            <v-card-subtitle>{{
-                userStore.userState.username
-            }}</v-card-subtitle>
+            <v-card-title>{{ userStore.userState.username }}</v-card-title>
+            <v-divider />
             <v-card-text>
                 <div>Roles</div>
                 <v-list>
                     <v-list-item
                         v-for="role in userStore.userState.roles"
                         :key="role"
-                    >
-                        {{ role }}
-                    </v-list-item>
-                </v-list>
-            </v-card-text></v-card
-        >
+                        :title="role"
+                    />
+                </v-list> </v-card-text
+        ></v-card>
     </v-menu>
 </template>
 
