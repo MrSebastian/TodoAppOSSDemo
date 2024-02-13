cb09050b677a1f86f343b4424b0d4dad899497bb
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2024-02-13 23:06:38 +0100

    Erstellung erster Komponenten und View zu Personen

---
+++ frontend/frontend/src/features/person/components/BasePersonList.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-list> </v-list>
+</template>
+
+<script setup lang="ts">
+import type PersonPersisted from "@/features/person/types/PersonPersisted";
+
+interface IProps {
+    persons: PersonPersisted[];
+}
+
+const props = defineProps<IProps>();
+</script>
+
+<style scoped></style>
---
+++ frontend/frontend/src/features/person/components/BasePersonListItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-list-item
+        :title="fullname"
+        :subtitle="props.person.id"
+    ></v-list-item>
+</template>
+
+<script setup lang="ts">
+import type PersonPersisted from "@/features/person/types/PersonPersisted";
+
+interface IProps {
+    person: PersonPersisted;
+}
+
+const props = defineProps<IProps>();
+
+function fullname(): string {
+    return `${props.person.firstname ?? ""} ${props.person.lastname ?? ""}`;
+}
+</script>
+
+<style scoped></style>
