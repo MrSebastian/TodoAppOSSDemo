eb1a438bedb0cfd73c4dedb1fe590f25e667844c
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2024-02-20 21:16:55 +0100

    new components to create a person

---
+++ frontend/frontend/src/features/person/components/BasePersonAddButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-btn>Person erstellen</v-btn>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ frontend/frontend/src/features/person/components/BasePersonFields.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <v-text-field
+            v-model="person.email"
+            label="EMail"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+import type Person from "@/features/person/types/Person";
+
+import { computed } from "vue";
+
+interface IProps {
+    person: Person;
+}
+
+const props = defineProps<IProps>();
+const emit = defineEmits<{
+    (e: "update:modelValue", value: Person): void;
+}>();
+
+const person = computed({
+    get: () => props.person,
+    set: (newValue: Person) => emit("update:modelValue", newValue),
+});
+</script>
+
+<style scoped></style>
