c2848aab2b119d3314df2f68735ee58261b2f4df
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2024-02-20 21:18:07 +0100

    add TODO marker for known problem in PersonPersisted.ts

--- frontend/frontend/src/features/person/types/PersonPersisted.ts
+++ frontend/frontend/src/features/person/types/PersonPersisted.ts
@@ -2,7 +2,7 @@ export default class PersonPersisted {
     constructor(
         public readonly id: string,
         public email: string,
-        public firstname?: string,
-        public lastname?: string
+        public firstname?: string, //TODO possible null from backend
+        public lastname?: string //TODO possible null from backend
     ) {}
 }
