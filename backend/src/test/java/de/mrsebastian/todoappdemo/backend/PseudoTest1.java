f459022cb7e4dbe51d938a6d5638c1b8d8161a30
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2024-02-21 20:50:03 +0100

    more pseudo stuff for 1

--- backend/src/test/java/de/mrsebastian/todoappdemo/backend/PseudoTest1.java
+++ backend/src/test/java/de/mrsebastian/todoappdemo/backend/PseudoTest1.java
@@ -1,5 +1,6 @@
 package de.mrsebastian.todoappdemo.backend;
 
+import lombok.val;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,5 +9,12 @@ public class PseudoTest1 {
     @Test
     void assertSthIsTrue() {
         Assertions.assertTrue(0 >= Math.random());
+    }
+
+    @Test
+    void assertMoreStuff() {
+        val value1 = Math.random();
+        val value2 = Math.random();
+        Assertions.assertTrue(value1 == value2 || value1 != value2);
     }
 }
