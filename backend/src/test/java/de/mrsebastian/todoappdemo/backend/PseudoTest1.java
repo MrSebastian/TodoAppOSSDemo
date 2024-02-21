d2797c66df71642b2466ffa2459ae09a01a06273
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2024-02-21 20:45:59 +0100

    add pseudo test

---
+++ backend/src/test/java/de/mrsebastian/todoappdemo/backend/PseudoTest1.java
@@ -0,0 +1,12 @@
+package de.mrsebastian.todoappdemo.backend;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PseudoTest1 {
+
+    @Test
+    void assertSthIsTrue() {
+        Assertions.assertTrue(0 >= Math.random());
+    }
+}
