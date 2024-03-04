e90afcd68fe93e9f6b31138e930d7a09311abc37
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2024-03-04 20:22:49 +0100

    create selector for a person

---
+++ frontend/frontend/src/features/person/components/BasePersonSelector.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-autocomplete
+        :items="props.availablePersons"
+        :item-title="'email'"
+        :item-value="'id'"
+    >
+    </v-autocomplete>
+</template>
+
+<script setup lang="ts">
+import type PersonPersisted from "@/features/person/types/PersonPersisted";
+
+interface IProps {
+    availablePersons: Array<PersonPersisted>;
+}
+
+const props = withDefaults(defineProps<IProps>(), {
+    availablePersons: () => [],
+});
+</script>
+
+<style scoped></style>
