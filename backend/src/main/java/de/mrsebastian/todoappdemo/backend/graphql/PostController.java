b0b8f060d0444d5d16a55731ccd9fbfd9bcad8fe
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2024-03-09 00:23:09 +0100

    extends GraphQL-Model and use a separate model for graphql; extend TaskService to get Tasks with specific assignee

--- backend/src/main/java/de/mrsebastian/todoappdemo/backend/graphql/PostController.java
+++ backend/src/main/java/de/mrsebastian/todoappdemo/backend/graphql/PostController.java
@@ -1,29 +1,48 @@
 package de.mrsebastian.todoappdemo.backend.graphql;
 
-import de.mrsebastian.todoappdemo.backend.person.rest.PersonDTO;
+import de.mrsebastian.todoappdemo.backend.graphql.types.Person;
+import de.mrsebastian.todoappdemo.backend.graphql.types.Task;
+import de.mrsebastian.todoappdemo.backend.graphql.types.TaskOfPerson;
+import de.mrsebastian.todoappdemo.backend.graphql.types.TypeMapper;
 import de.mrsebastian.todoappdemo.backend.person.service.PersonService;
-import de.mrsebastian.todoappdemo.backend.task.rest.TaskDTO;
 import de.mrsebastian.todoappdemo.backend.task.service.TaskService;
+import java.util.Collections;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class PostController {
 
     private final TaskService taskService;
 
     private final PersonService personService;
 
+    private final TypeMapper typeMapper;
+
     @QueryMapping
-    public List<TaskDTO> getTasks() {
-        return taskService.getTasks();
+    public List<Task> getTasks() {
+        return taskService.getTasks().stream().map(taskDTO -> typeMapper.taskDtoToTaskType(taskDTO, null)).toList();
     }
 
     @QueryMapping
-    public List<PersonDTO> getPersons() {
-        return personService.getPersonen();
+    public List<Person> getPersons() {
+        return personService.getPersonen().stream().map(typeMapper::personDtoToPersonType).toList();
+    }
+
+    @SchemaMapping
+    public List<TaskOfPerson> tasks(Person person) {
+        log.atInfo().log("requesting tasks of person > {}", person);
+
+        if (person.id() == null) {
+            return Collections.emptyList();
+        }
+
+        return taskService.getTaskWithAssignee(person.id()).stream().map(typeMapper::taskDtoToTaskOfPersonType).toList();
     }
 }
