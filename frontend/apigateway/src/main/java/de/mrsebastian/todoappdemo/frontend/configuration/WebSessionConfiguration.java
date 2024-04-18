e786e95701f41da046509d11e5e0373d57c87644
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2024-04-19 00:09:36 +0200

    spotless:apply

--- frontend/apigateway/src/main/java/de/mrsebastian/todoappdemo/frontend/configuration/WebSessionConfiguration.java
+++ frontend/apigateway/src/main/java/de/mrsebastian/todoappdemo/frontend/configuration/WebSessionConfiguration.java
@@ -26,7 +26,6 @@ import org.springframework.session.hazelcast.HazelcastIndexedSessionRepository;
 
 /**
  * This class configures Hazelcast as the ReactiveSessionRepository.
- *
  */
 @Configuration
 @EnableSpringWebSession
@@ -98,7 +97,7 @@ public class WebSessionConfiguration {
 
     /**
      * Adds the session timeout in seconds to the hazelcast configuration.
-     *
+     * <p>
      * Since we are creating the map it's important to evict sessions
      * by setting a reasonable value for time to live.
      *
