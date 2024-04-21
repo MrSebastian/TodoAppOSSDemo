4f7671352cf3bd8a6c4120828e3cc51b31ef8da4
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2024-04-21 18:07:29 +0200

    adapt testapplication component to reordered package structure

--- backend-rest-api/task/src/test/java/de/mrsebastian/todoappdemo/task/api/rest/util/TestApplication.java
+++ backend-rest-api/task/src/test/java/de/mrsebastian/todoappdemo/task/api/rest/util/TestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("de.mrsebastian.todoappdemo.backend.task.api.rest")
+@ComponentScan("de.mrsebastian.todoappdemo.task.api.rest")
 public class TestApplication {
 
     public static void main(String[] args) {
