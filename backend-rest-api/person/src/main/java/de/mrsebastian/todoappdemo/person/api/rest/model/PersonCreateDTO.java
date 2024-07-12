a27e2412789cfd444cd25c3fe3118aa5cf61f54a
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2024-07-12 18:10:56 +0200

    remove separate api project. not public deployable at the moment

--- backend-rest-api/person/src/main/java/de/mrsebastian/todoappdemo/person/api/rest/model/PersonCreateDTO.java
+++
@@ -1,9 +0,0 @@
-package de.mrsebastian.todoappdemo.person.api.rest.model;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
-
-@Builder
-public record PersonCreateDTO(String firstname, String lastname, @NotNull @Email String email) {
-}
--- backend-rest-api/task/src/main/java/de/mrsebastian/todoappdemo/task/api/rest/model/TaskUpdateDTO.java
+++
@@ -1,9 +0,0 @@
-package de.mrsebastian.todoappdemo.task.api.rest.model;
-
-import jakarta.validation.constraints.NotNull;
-import java.time.LocalDate;
-import lombok.Builder;
-
-@Builder
-public record TaskUpdateDTO(@NotNull String title, @NotNull String description, LocalDate dueDate) {
-}
